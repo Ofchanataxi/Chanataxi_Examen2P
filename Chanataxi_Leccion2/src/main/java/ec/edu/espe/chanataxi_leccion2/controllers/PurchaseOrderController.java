8f8aa9401f91a50230f46c3c28915954a363ca3a
Oscarf05 <ofchanataxi@espe.edu.ec>
2025-12-15 17:55:30 -0500

    Agregar docker

--- Chanataxi_Leccion2/src/main/java/ec/edu/espe/chanataxi_leccion2/controllers/PurchaseOrderController.java
+++ Chanataxi_Leccion2/src/main/java/ec/edu/espe/chanataxi_leccion2/controllers/PurchaseOrderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/purchase-orders")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class PurchaseOrderController {
 
     @Autowired
